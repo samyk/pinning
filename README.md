e87680d54786316e30378b8c1979684553ad3a50
Samy Kamkar <git@samy.pl>
2016-11-28 22:13:00 -0800

    New todos and cleanup

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # #PINNING
-
 ===========
-
 \#PINNING is a browser extension (currently TamperMonkey script) to improve Pinterest's desktop browsing UX.
 
 ## Features
@@ -16,16 +14,21 @@
 	* Use left/right arrow keys to traverse pins (in addition to standard hover)
 
 ## Todo
-* Fix bug where you must 'click' on the page before being able to use hotkeys
-* When traversing, page should autoscroll to display full pin
+* Add [Computer Vision API](https://www.microsoft.com/cognitive-services/en-us/Computer-Vision-API/documentation/DescribingImages) to automatically detect which board to pin to
+* Fix bug where you must click on the page before being able to use hotkeys
+* When traversing via arrowkeys, page should autoscroll to display full pin
+* When hovering over pin, page should autoscroll to display full pin
 * When saving to board (hitting 'S' or **rolling over** 'Save' button), big modal should **not** popup, instead a small rollover should display beneath Save button with list of all boards
+  * Clicking Save can still open modal for backwards compatibility, though unnecessary as rolling over should expose all boards to save to
 	* Include typeahead and up/down arrow support for board selection
 	* Don't show duplicate boards
 	* Display last saved board, or perhaps last 3 saved to boards on top
 	* Make the display small so you still see the rest of pins
 	* When finally saving, the save should happen in the background so you can immediately continue browsing pins
-* When Liking selected pin, show the like button (currently hidden as the pin is not actually in :hover state)
+* When a pin is selected, show the buttons (currently hidden as the pin is not actually in :hover state)
 * Once in a good state, drop into its own extension
+* Remove the width from border around pins (outline vs border?)
+* Select the first item on the page by default
 
 ## Installation
 
@@ -39,4 +42,5 @@
 ## Contact
 * [https://samy.pl](https://samy.pl)
 * [@SamyKamkar](https://twitter.com/samykamkar/)
+* [https://pinterest.com/samyk](https://pinterest.com/samyk)
 
--- pinning.js
+++ pinning.js
@@ -2,101 +2,147 @@
 // @name         #PINNING
 // @namespace    https://samy.pl
 // @version      0.1
-// @description  PINNING
+// @description  #PINNING
 // @author       Samy Kamkar
 // @match        https://www.pinterest.com/
 // @grant        none
-// @require https://code.jquery.com/jquery-3.1.1.min.js
+// @require      https://code.jquery.com/jquery-3.1.1.min.js
 // ==/UserScript==
 
-// On hover, data-is-hovering="true"
-
 (function() {
-    // currently selected item
-    var item;
-    
-    // Remove ads/promoted content
+  // currently selected item
+  var item;
+
+  // setup
+  removeAds();   // remove ads
+  addHotkeys();  // add hotkeys to the page
+  detectHover(); // select pin when hovered over
+  selectItem($('.item:first')); // select the first item
+
+  // Remove ads/promoted content
+  //
+  // XXX this needs to happen when new content is loaded -
+  // perhaps overload XHR to detect when this happens
+  // or when window size changes
+  // also see https://github.com/marcj/css-element-queries
+  /*
+  var body = document.body,
+      html = document.documentElement;
+  var height = Math.max(
+    body.scrollHeight, body.offsetHeight,
+    html.clientHeight, html.scrollHeight, html.offsetHeight);
+  */
+  function removeAds()
+  {
+    // XXX should add a class (eg 'noAd') so those don't get searched in the future
     for (var x of document.getElementsByClassName('creditName'))
         if (x.innerText.indexOf('Promoted by') === 0)
         {
             var tmp = x.closest('.item');
-            if (tmp) tmp.style.visibility = 'hidden';
+            if (tmp)
+              tmp.style.visibility = 'hidden';
         }
+  }
 
-    // Add border to selected pin, remove border on previous
-    function setBorders(newItem)
-    {
-        if (newItem)
-        {
-            // Remove border from old item
-            if (item)
-            {
-                item.style.border = '0px';
-                item.setAttribute("data-is-hovering", "false");
-            }
+  // Select a pin, add border to pin, remove border from previous
+  function selectItem(newItem)
+  {
+      if (newItem)
+      {
+          // Remove border from old item
+          if (item)
+          {
+              //item.style.border = '0px';
+              item.style.outline = '0px';
 
-            item = newItem;
-            item.setAttribute("data-is-hovering", "true");
-            console.log(findPos(item), item);
-            // window.scroll(0, findPos(item));
-            item.focus();
-            item.style.border = '2px solid #cb2027';
-            item.style.borderRadius = '10px';
-        }
-    }
+              // don't think this really does anything
+              item.setAttribute("data-is-hovering", "false");
+          }
 
-    // Find the current bounds of the image in the page to help scrolling
-    function findPos(obj)
-    {
-        var curtop = 0;
-        if (obj.offsetParent)
-        {
-            do
-            {
-                obj = obj.offsetParent;
-                curtop += obj.offsetTop;
-            } while (obj.offsetParent);
-            return curtop;//[curtop]
-        }
-    }
+          // better way to handle this? w/jquery?
+          item = newItem.setAttribute ? newItem : newItem[0];
 
-    // Find the item (pin) user is hovering mouse over
+          // XXX if window doesn't have full view of pin, scroll
+          // so that window has full view of pin (if pin is below,
+          // top of pin should be at top of page, but if pin is
+          // above, window should scroll so bottom of pin is at
+          // bottom of window to stay closest to current position)
+          // window.scroll(0, findPos(item));
+
+          // don't think this really does anything
+          item.setAttribute("data-is-hovering", "true");
+
+          //item.style.border = '2px solid #cb2027';
+          //item.style.borderRadius = '10px';
+          //-webkit-border-radius
+          item.style.boxShadow = '0 0 3px #ddd';
+          item.style.outlineRadius = '10px';
+          item.style.outline = '2px solid #cb2027';
+
+          // don't think this really does anything
+          item.focus();
+      }
+  }
+
+  // Find the current bounds of the image in the page to help scrolling
+  function findPos(obj)
+  {
+      var curtop = 0;
+      if (obj.offsetParent)
+      {
+          do
+          {
+              obj = obj.offsetParent;
+              curtop += obj.offsetTop;
+          } while (obj.offsetParent);
+          return curtop;//[curtop]
+      }
+  }
+
+  // Find the item (pin) user is hovering mouse over
+  function detectHover()
+  {
     window.onmouseover = function(e)
     {
-        setBorders(e.target.closest('.item'));
+        // Find the closest (parent) .item and select it
+        selectItem(e.target.closest('.item'));
     };
+  }
 
-    // Hotkey support
+  // Hotkey support
+  function addHotkeys()
+  {
+    $(window).focus();
     $(window).keydown(function(e)
     {
         switch (e.keyCode)
         {
-            case 76:  // L
+            case 76:  // L - like/heart
             case 108: // l
                 item.querySelectorAll('.LikeButton')[0].click();
                 break;
-            case 70:  // F
+            case 70:  // F - forward
             case 102: // f
                 item.querySelectorAll('.DropdownButton')[0].click();
                 break;
-            case 83:  // S
+            case 83:  // S - save to board (pin)
             case 115: // s
                 item.querySelectorAll('.ShowModalButton')[0].click();
                 break;
-            case 86:  // V
+            case 86:  // V - visit in new window
             case 118: // v
                 item.querySelectorAll('.NavigateButton')[0].click();
                 break;
-            case 13: // Enter
+            case 13: // Enter - expand pin
                 item.querySelectorAll('.pinImageWrapper')[0].click();
                 break;
-            case 37: // left arrow
-                setBorders(item.previousElementSibling);
+            case 37: // left arrow - select previous pin
+                selectItem(item.previousElementSibling);
                 break;
-            case 39: // right arrow
-                setBorders(item.nextElementSibling);
+            case 39: // right arrow - select next pin
+                selectItem(item.nextElementSibling);
                 break;
         }
     });
-
+  }
 })();
